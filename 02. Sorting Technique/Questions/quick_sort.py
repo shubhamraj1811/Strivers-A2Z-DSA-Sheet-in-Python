7245fb87f0143b4df91499c971ffa077697242cd
Shubham Raj <ershubhcsofficial@gmail.com>
2026-07-29 08:50:21 +0530

    array: Largest Element

--- 02. Sorting Technique/Questions/quick_sort.py
+++ 02. Sorting Technique/Questions/quick_sort.py
@@ -1,3 +1,15 @@
+# ===== QUICK SORT =====
+def quickSort(ar, low, high):
+    if low < high:
+        # get pivot index
+        p_index = partition(ar, low, high)
+
+        # left unsorted part
+        quickSort(ar, low, p_index-1)
+
+        # right unsorted part
+        quickSort(ar, p_index+1, high)
+
 def partition(ar, low, high):
     # take a pivot
     pivot = ar[low]
@@ -17,20 +29,7 @@ def partition(ar, low, high):
     ar[low], ar[j] = ar[j], ar[low]
     return j
 
-# ===== QUICK SORT =====
-def quickSort(ar, low, high):
-    if low < high:
-        # get pivot index
-        p_index = partition(ar, low, high)
-
-        # left unsorted part
-        quickSort(ar, low, p_index-1)
-
-        # right unsorted part
-        quickSort(ar, p_index+1, high)
-
 # main
-
 ar = [3, 1, 2, 4, 7, 6, 8]
 n = len(ar)
 quickSort(ar, 0, n-1)
---
+++ 03. Arrays/Easy/largest_element.py
@@ -0,0 +1,38 @@
+"""
+❇️ Given an array of integers nums, return the value of the largest element in the array
+
+💭 Example:
+Input: nums = [3, 3, 6, 1]
+Output: 6
+Explanation: The largest element in array is 6
+"""
+
+# ====== first method - linear search ======
+def findLargestElement (ar):
+    # edge case - empty array
+    if not ar:
+        return None
+    
+    large = ar[0]
+    for num in ar:
+        if num > large:
+            large = num
+
+    return large
+
+# Time = O(n)
+# Space = O(1)
+
+# ====== second method - max function ======
+def pythonMethod(ar):
+    return max(ar, default=None)
+
+# ====== main ======
+ar = [2, 3, 41, 6, 0, 9]
+largest1 = findLargestElement(ar)
+print(f"Largest element in the array is: {largest1}")
+
+print()
+
+largest2 = pythonMethod(ar)
+print(f"Largest element in the array is (MAX FUN): {largest2}")
